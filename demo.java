ca8a40c5d9ca2ad5ab99862bc71cc7de36534c45
Susana2004 <gs6986164@gmail.com>
2025-04-23 14:30:14 +0530

    javaprogram

---
+++ demo.java
@@ -0,0 +1,11 @@
+public class demo{
+    public static void main(String[] args) {
+        int a=4;
+        int b=2;
+        int sum =a+b;
+        int sub =a-b;
+        System.out.println("Addition:" +sum);
+        System.out.println("Subtraction:" +sub);
+        
+    }
+}
